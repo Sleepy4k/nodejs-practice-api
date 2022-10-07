08ff619232104c794b7c01153042e22a5bbb1303
Sleepy4k <sarahpalastrin@gmail.com>
2022-10-07 08:44:39 +0700

    feat: practice 4

---
+++ History/praktik_4.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const todos = [
+    {id: 1, message: 'Todo Satu'},
+    {id: 2, message: 'Todo Dua'},
+    {id: 3, message: 'Todo Tiga'},
+];
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+    })
+
+    const data = JSON.stringify({
+       status: 200,
+       data: todos 
+    });
+
+    res.end(data);
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server berjalan pada port ${PORT}`));
