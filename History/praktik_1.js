15de56121733caa552a55e734bb673f5dc3db466
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-12 22:25:57 +0700

    feat!: old source
    remove old source code api

--- History/praktik_1.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    console.log(req);
-});
-
-const PORT = 5000;
-
-server.listen(PORT, () => console.log(`Server berjalan pada port ${PORT}`));
