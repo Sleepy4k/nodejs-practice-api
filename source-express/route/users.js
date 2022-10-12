f9be98419da588cc41b58527341413af7f9289e6
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-12 22:28:21 +0700

    feat: express source
    add main express source code

---
+++ source-express/route/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// Controller
+const userController = require('../controller/users')
+
+// Main Route
+router.get('/users', userController.index)
+router.post('/user', userController.store)
+router.put('/user/:id', userController.update)
+router.delete('/user/:id', userController.delete)
+
+// Module Export
+module.exports = router
