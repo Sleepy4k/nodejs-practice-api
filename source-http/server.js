d712ce2cf1b54fbdcedc21454af18d2ac2574a0e
Sleepy4k <sarahpalastrin@gmail.com>
2022-10-14 11:25:56 +0700

    feat: server http
    change old method with new method and clean up code

--- source-http/server.js
+++ source-http/server.js
@@ -1,12 +1,28 @@
 const http = require('http');
-const PORT = 5000;
+const fs = require('fs');
 
-// Data Dummy
-const dataTugas = [
-    {urutan: 1, tugas: 'Tugas Satu'},
-    {urutan: 2, tugas: 'Tugas Dua'},
-    {urutan: 3, tugas: 'Tugas Tiga'},
-];
+// Get Config File
+require('dotenv/config');
+
+// Const for listen web
+const name = process.env.APP_NAME
+const port = process.env.APP_PORT
+
+// Database Location
+const tugasData = `./${process.env.DB_PATH}/tugas.json`
+
+// Read Database User File
+function getTugasData() {
+    const jsonData = fs.readFileSync(tugasData)
+
+    return JSON.parse(jsonData)
+}
+
+// Write Database User File
+function saveTugasData(body) {
+    const stringifyData = JSON.stringify(body)
+    fs.writeFileSync(tugasData, stringifyData)
+}
 
 // Main Function Create Server
 const server = http.createServer((permintaan, respon) => {
@@ -19,38 +35,59 @@ const server = http.createServer((permintaan, respon) => {
     }).on('end', () => {
         body = Buffer.concat(body).toString();
 
-        let status = 200;
+        let code = 200;
+        let status = true;
         const response = {
             status: status,
-            results: [],
+            message: '',
+            meta: {
+                hostname: permintaan.hostname,
+                method: permintaan.method,
+                url: permintaan.url
+            },
+            data: [],
             error: ''
         };
 
         if (url === '/tugas') {
             if (method === 'GET') {
-                response.results = dataTugas;
+                response.data = getTugasData();
             } else if (method === 'POST') {
                 const {urutan, tugas} = JSON.parse(body);
                 if (!urutan || !tugas) {
-                    response.status = 400;
+                    code = 400;
+                    status = false;
                     response.error = 'Silahkan masukan data urutan dan tugas yang benar';
                 } else {
-                    dataTugas.push({urutan, tugas});
-                    response.status = 201;
-                    response.results = dataTugas;
+                    const object = getTugasData()
+
+                    if (object.find(data => data.urutan == urutan)) {
+                        status = false;
+                        code = 500;
+                        response.message = 'proses gagal'
+                        response.error = 'Data urutan terduplikat';
+
+                        return respon.end(JSON.stringify(response))
+                    }
+
+                    object.push({urutan, tugas});
+
+                    saveTugasData(object);
+
+                    code = 201;
+                    response.results = getTugasData();
                 }
             }
         }
 
-        respon.writeHead(status, {
+        respon.writeHead(code, {
             'Content-Type': 'application/json',
             'X-Powered-By': 'Node.js'
         })
 
         respon.end(JSON.stringify(response));
-
     });
 });
 
 // Listen Web
-server.listen(PORT, () => console.log(`Server berjalan pada port ${PORT}`));
+server.listen(port, () => console.log(`${name} berjalan pada port ${port}`));
