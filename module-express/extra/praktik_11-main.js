05efec15ddbfc815fba3e730bb4f4929fdf46c00
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-15 09:57:07 +0700

    feat: extra express module
    add new module extra due fix bug when update data and delete

---
+++ module-express/extra/praktik_11-main.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+
+// Get Config File
+require('dotenv/config')
+
+// Const for config
+var name = process.env.APP_NAME || 'Express'
+var port = process.env.APP_PORT || '3000'
+var routePath = process.env.PATH_ROUTER || 'routes'
+
+// List Route
+var users = require(`./${routePath}/users`)
+
+// Read Form Request
+app.use(express.json())
+
+// Read Encode Form Request
+app.use(express.urlencoded({ extended: true }))
+
+// Main Web Route
+app.get('/', (permintaan, respon) => {
+    respon.send('Halo Dunia!')
+})
+
+app.use(users)
+
+// Listen Web
+app.listen(port, () => {
+    console.log(`${name} listening on port ${port}`)
+})
