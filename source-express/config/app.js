3c4384e439604ed8a59c1b1f09204ad028273373
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-16 10:21:16 +0700

    feat: express app config
    add app locale config

--- source-express/config/app.js
+++ source-express/config/app.js
@@ -51,5 +51,18 @@ module.exports = {
     |
     */
 
-    url: process.env.APP_URL || 'http://localhost:3000'
+    url: process.env.APP_URL || 'http://localhost:3000',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
+
+    locale: process.env.APP_LOCALE || 'en',
 };
