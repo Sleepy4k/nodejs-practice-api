a58c9f4b270655e6b617297412948f6ff87999cf
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-25 20:58:21 +0700

    feat: express repositories
    add dynamic database type

--- source-express/app/repositories/TaskRepository.js
+++ source-express/app/repositories/TaskRepository.js
@@ -1,38 +1,100 @@
 // Get Config File
 var { system } = require('../../config/path')
+var database = require('../../config/database')
 
-// Get users model
+// Get tasks model
 var model = require(`../${system.models}/tasks`)
 
-module.exports = {
-    /**
-     * Display a listing of the model.
-     * 
-     * @return Array
-     */
-    getTaskData: function() {
-        return model.get()
-    },
+if (database.default == 'file') {
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        getTaskData: function() {
+            return model.get()
+        },
+        
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        saveTaskData: function(body) {
+            return model.store(body)
+        },
     
-    /**
-     * Store a newly created resource in model.
-     *
-     * @param Object body
-     * 
-     * @return Bool
-     */
-    saveTaskData: function(body) {
-        return model.store(body)
-    },
-
-    /**
-     * Display a specified model.
-     *
-     * @param int id
-     * 
-     * @return Array
-     */
-    findTaskData: function(id) {
-        return model.find(id)
-    }
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        findTaskData: function(id) {
+            return model.find(id)
+        }
+    } 
+} else if (database.default == 'mongoose') {
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        getTaskData: function() {
+            return model.find()
+        },
+        
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        saveTaskData: function(body) {
+            return model.create(body)
+        },
+    
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        findTaskData: function(id) {
+            return model.findById(id)
+        },
+    
+        /**
+         * Update the specified resource in model.
+         *
+         * @param int id
+         * @param Object body
+         * 
+         * @return Array
+         */
+        updateTaskData: function(id, body) {
+            return model.findByIdAndUpdate(id, body, {
+                new: true,
+                runValidators: true
+            })
+        },
+    
+        /**
+         * Remove the specified resource from model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        destroyTaskData: function(id) {
+            return model.findByIdAndDelete(id)
+        }
+    } 
 }
--- source-express/app/repositories/UserRepository.js
+++ source-express/app/repositories/UserRepository.js
@@ -1,38 +1,100 @@
 // Get Config File
 var { system } = require('../../config/path')
+var database = require('../../config/database')
 
 // Get users model
 var model = require(`../${system.models}/users`)
 
-module.exports = {
-    /**
-     * Display a listing of the model.
-     * 
-     * @return Array
-     */
-    getUserData: function() {
-        return model.get()
-    },
+if (database.default == 'file') {
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        getUserData: function() {
+            return model.get()
+        },
+        
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        saveUserData: function(body) {
+            return model.store(body)
+        },
     
-    /**
-     * Store a newly created resource in model.
-     *
-     * @param Object body
-     * 
-     * @return Bool
-     */
-    saveUserData: function(body) {
-        return model.store(body)
-    },
-
-    /**
-     * Display a specified model.
-     *
-     * @param int id
-     * 
-     * @return Array
-     */
-    findUserData: function(id) {
-        return model.find(id)
-    }
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        findUserData: function(id) {
+            return model.find(id)
+        }
+    } 
+} else if (database.default == 'mongoose') {
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        getUserData: function() {
+            return model.find()
+        },
+        
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        saveUserData: function(body) {
+            return model.create(body)
+        },
+    
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        findUserData: function(id) {
+            return model.findById(id)
+        },
+    
+        /**
+         * Update the specified resource in model.
+         *
+         * @param int id
+         * @param Object body
+         * 
+         * @return Array
+         */
+        updateUserData: function(id, body) {
+            return model.findByIdAndUpdate(id, body, {
+                new: true,
+                runValidators: true
+            })
+        },
+    
+        /**
+         * Remove the specified resource from model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        destroyUserData: function(id) {
+            return model.findByIdAndDelete(id)
+        }
+    } 
 }
