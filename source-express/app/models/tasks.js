43d9a01c20a015231697eb4a95bd6298ec9b140c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-25 20:57:19 +0700

    feat: express models
    add dynamic database type

--- source-express/app/models/tasks.js
+++ source-express/app/models/tasks.js
@@ -1,47 +1,62 @@
 // Get Config File
 var { system } = require('../../config/path')
+var database = require('../../config/database')
 
 // Get model handler
 var model = require(`../http/${system.modules}/model`)
 
-// Database Column
-var column = 'tasks'
+if (database.default == 'file') {
+    // Database Column
+    var column = 'tasks'
 
-module.exports = {
-    /**
-     * Display a listing of the model.
-     * 
-     * @return Array
-     */
-    get: function() {
-        if (model.exist(column)) {
-            return model.get(column)
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        get: function() {
+            if (model.exist(column)) {
+                return model.get(column)
+            }
+        },
+    
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        store: function(data) {
+            if (model.exist(column)) {
+                return model.store(column, data)
+            }
+        },
+    
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        find: function(id) {
+            if (model.exist(column)) {
+                return model.find(column, id)
+            }
         }
-    },
+    }   
+} else if (database.default == 'mongoose') {
+    // Export Mongoose
+    var mongoose = require('mongoose')
 
-    /**
-     * Store a newly created resource in model.
-     *
-     * @param Object body
-     * 
-     * @return Bool
-     */
-    store: function(data) {
-        if (model.exist(column)) {
-            return model.store(column, data)
+    // Mongoose Init
+    module.exports = mongoose.model('tasks', new mongoose.Schema({
+        tugas: {
+            type: String,
+            required: [true, 'Silahkan isikan tugas'],
+            unique: false
         }
-    },
-
-    /**
-     * Display a specified model.
-     *
-     * @param int id
-     * 
-     * @return Array
-     */
-    find: function(id) {
-        if (model.exist(column)) {
-            return model.find(column, id)
-        }
-    }
+    }))
 }
--- source-express/app/models/users.js
+++ source-express/app/models/users.js
@@ -1,47 +1,68 @@
 // Get Config File
 var { system } = require('../../config/path')
+var database = require('../../config/database')
 
 // Get model handler
 var model = require(`../http/${system.modules}/model`)
 
-// Database Column
-var column = 'users'
+if (database.default == 'file') {
+    // Database Column
+    var column = 'users'
 
-module.exports = {
-    /**
-     * Display a listing of the model.
-     * 
-     * @return Array
-     */
-    get: function() {
-        if (model.exist(column)) {
-            return model.get(column)
+    module.exports = {
+        /**
+         * Display a listing of the model.
+         * 
+         * @return Array
+         */
+        get: function() {
+            if (model.exist(column)) {
+                return model.get(column)
+            }
+        },
+    
+        /**
+         * Store a newly created resource in model.
+         *
+         * @param Object body
+         * 
+         * @return Bool
+         */
+        store: function(data) {
+            if (model.exist(column)) {
+                return model.store(column, data)
+            }
+        },
+    
+        /**
+         * Display a specified model.
+         *
+         * @param int id
+         * 
+         * @return Array
+         */
+        find: function(id) {
+            if (model.exist(column)) {
+                return model.find(column, id)
+            }
         }
-    },
+    }   
+} else if (database.default == 'mongoose') {
+    // Export Mongoose
+    var mongoose = require('mongoose')
 
-    /**
-     * Store a newly created resource in model.
-     *
-     * @param Object body
-     * 
-     * @return Bool
-     */
-    store: function(data) {
-        if (model.exist(column)) {
-            return model.store(column, data)
+    // Mongoose Init
+    module.exports = mongoose.model('users', new mongoose.Schema({
+        nama: {
+            type: String,
+            required: [true, 'Silahkan isikan nama'],
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Silahkan isikan email valid']
         }
-    },
-
-    /**
-     * Display a specified model.
-     *
-     * @param int id
-     * 
-     * @return Array
-     */
-    find: function(id) {
-        if (model.exist(column)) {
-            return model.find(column, id)
-        }
-    }
+    }))
 }
